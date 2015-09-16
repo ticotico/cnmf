25f317b9f909a1adc34ce6aa1f023b9091d90269
ticotico <beranbus@gmail.com>
2015-09-16 22:38:27 +0800

    Create useage.c

---
+++ useage.c
@@ -0,0 +1,13 @@
+#include <stdio.h> 
+
+void useage()
+{
+	printf("-------------------------------------------------------------\n");
+	printf("cnmf v1.02 test version.\n");
+	printf("Useage: cnmf <command> <filename> <lines1> <lines2> ...\n");
+	printf("\n<commands>\n");
+	printf("getfll\t\tget first specific lines in logfile.\n");
+	printf("getall\t\tget all specific lines in logfile.\n");
+
+	printf("-------------------------------------------------------------\n");
+}
